f57dafaf8898b7769210ebd49b47e7b650cefa59
stacepsho <stacepsho@gmail.com>
2025-02-11 12:00:43 +0800

    20250211 app.py

--- app.py
+++ app.py
@@ -11,34 +11,34 @@ received_data = []
 @app.route('/postData', methods=['POST'])
 def receive_data():
     if request.content_type == 'application/x-www-form-urlencoded':
-        print("---x-www-form-urlencoded---")
+        print("---x-www-form-urlencoded---", flush=True)
         data = request.form
         if data:
             # 列出所有的鍵值對
             key_value_pairs = {key: data[key] for key in data}
-            print("---Received Key-Value Pairs---")
+            print("---Received Key-Value Pairs---", flush=True)
             for key, value in key_value_pairs.items():
-                print(f"{key}: {value}")
-            return jsonify({"message": "Data received"}), 201
+                print(f"{key}: {value}", flush=True)
+            return jsonify({"message": "Data received"}), 200
         else:
             return jsonify({"error": "No data received"}), 400
     elif request.content_type == 'application/json':
-        print("---json---")
+        print("---json---", flush=True)
         data = request.get_json()
         if data is None:
             return jsonify({"error": "Invalid JSON data"}), 400
     else:
-        print("---Unsupported Content-Type---")
+        print("---Unsupported Content-Type---", flush=True)
         return jsonify({"error": "Unsupported Content-Type"}), 415
     
     received_data.append(data)
-    #print(f"---Received data---: {data}")
-    return jsonify({"message": "Data received"}), 201
+    print(f"---Received data---: {data}", flush=True)
+    return jsonify({"message": "Data received"}), 200
 
-@app.route('/getData', methods=['GET'])
+@app.route('/getData', methods=['GET']), flush=True
 def list_data():
     # 在控制台顯示將要列出的資料
-    print(f"---Listing all received data---: {received_data}")
+    print(f"---Listing all received data---: {received_data}", flush=True)
     return jsonify(received_data), 200
 
 @app.route('/')
