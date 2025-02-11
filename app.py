1cbfc38aa5214daa4457bf156b0b0492a38cf7f8
stacepsho <stacepsho@gmail.com>
2025-02-11 11:36:53 +0800

    20250211 app.py

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 import os
+import json
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 received_data = []
 
 @app.route('/postData', methods=['POST'])
 def receive_data():
-    #接收資料
-    data = request.get_json()
+    if request.content_type == 'application/x-www-form-urlencoded':
+        data_str = request.form.get('data')
+        if data_str:
+            try:
+                data = json.loads(data_str)
+            except json.JSONDecodeError:
+                return jsonify({"error": "Invalid JSON data"}), 400
+        else:
+            return jsonify({"error": "No data provided"}), 400
+    elif request.content_type == 'application/json':
+        data = request.get_json()
+        if data is None:
+            return jsonify({"error": "Invalid JSON data"}), 400
+    else:
+        return jsonify({"error": "Unsupported Content-Type"}), 415
+    
     received_data.append(data)
-    # 在控制台顯示收到的資料
     print(f"---Received data---: {data}")
     return jsonify({"message": "Data received"}), 201
 
