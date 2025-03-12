11e62042fb16651b58d8402391564fa3e036dbb6
stacepsho <stacepsho@gmail.com>
2025-03-12 16:57:43 +0800

    20250312 app.py slash command

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 import os
 import json
 import requests
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 app = Flask(__name__)
 #CORS(app)
@@ -75,7 +77,7 @@ def receive_data():
 
 @app.route('/stopPTZ', methods=['POST'])
 def command_stopptz():
-    #SLASH COMMAND
+    #SLASH COMMAND 停止巡弋
     if request.content_type == 'application/x-www-form-urlencoded':
         print("---x-www-form-urlencoded---", flush=True)
         data = request.form
@@ -86,6 +88,11 @@ def command_stopptz():
             for key, value in key_value_pairs.items():
                 print(f"key is ：{key}", flush=True)
                 print(f"value is ：{value}", flush=True)
+            
+            #回應訊息到群組
+            msg ='[{"type":"section","text":{"type":"mrkdwn","text":":請指定*專案跟攝影機*"}},{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":"*<fakeLink.toYourApp.com|專案>*\n專案名稱"},"accessory":{"type":"static_select","placeholder":{"type":"plain_text","emoji":true,"text":"專案"},"options":[{"text":{"type":"plain_text","emoji":true,"text":"台中AI"},"value":"台中AI"},{"text":{"type":"plain_text","emoji":true,"text":"台中4合1"},"value":"台中4合1"},{"text":{"type":"plain_text","emoji":true,"text":"高雄AI"},"value":"高雄AI"}]}},{"type":"section","text":{"type":"mrkdwn","text":"*<fakeLink.toYourApp.com|攝影機>*\n攝影機"},"accessory":{"type":"static_select","placeholder":{"type":"plain_text","emoji":true,"text":"攝影機"},"options":[{"text":{"type":"plain_text","emoji":true,"text":"CAM_01"},"value":"CAM_01"},{"text":{"type":"plain_text","emoji":true,"text":"CAM_02"},"value":"CAM_02"},{"text":{"type":"plain_text","emoji":true,"text":"CAM_03"},"value":"CAM_03"}]}},{"type":"divider"},{"type":"actions","elements":[{"type":"button","text":{"type":"plain_text","emoji":true,"text":"送出"},"value":"送出"}]}]'
+            toSLACK("xoxb-8113930665923-8117072255540-cJeQj9SMJAsQnHOgYwzg3E78",'C083CDAMHNX',msg,"","","mybot",9999)
+
     elif request.content_type == 'application/json':
         print("---json---", flush=True)
         data = request.get_json()
@@ -105,6 +112,37 @@ def list_data():
 @app.route('/')
 def hello():
     return f'---RUNNING---'
+
+def toSLACK(slack_token, channel_id ,msg, img_file_path,video_file_path,bot_name,mid):
+    #
+    # 透過SLACK送出訊息
+    #
+    client = WebClient(token=slack_token)
+    try:
+        #response = client.chat_postMessage(channel=channel_id, text=msg) 
+        #blocks 是slack特定的文字區塊，採用json format
+        if "[{" in msg:
+            response = client.chat_postMessage(channel=channel_id, blocks=msg)
+        else:
+            response = client.chat_postMessage(channel=channel_id, text=msg) 
+
+        if(img_file_path!=''):
+            response = client.files_upload_v2(channel=channel_id,title="照片：",file=img_file_path,username=bot_name)
+            permalink = response['files'][0]['permalink']
+        if(video_file_path!=''):
+            response = client.files_upload_v2(channel=channel_id,title="影片：",file=video_file_path,username=bot_name)
+            permalink = response['files'][0]['permalink']
+
+        print(f"ID:{mid} Message post successfully!")
+        logging.info(f"ID:{mid} Message post successfully!")
+        #The response includes the "timestamp ID" (ts) and the channel-like thing where the message was posted
+        return int(response['ok']), ''
+        
+    except SlackApiError as e:
+        print(f"【錯誤】: {e.response['error']}")
+        logging.fatal(f"ID:{mid}. ERROR:{e.response['error']}")
+        return 0, e.response['error']
+
 
 if __name__ == '__main__':
     port_nr = int(os.environ.get("PORT", 10000))
